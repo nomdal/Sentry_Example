297c50786ff872e63062230b8f9c0917b223e552
Noah Omdal <njomdal@gmail.com>
2020-09-01 16:28:40 -0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: 'https://87d2dccd89a34d83a76a648bb9273337@o442275.ingest.sentry.io/5413790',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(err => console.error(err));
+
+
+
